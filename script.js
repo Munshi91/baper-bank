8056f9b5cd8632f46865103de2dec8e8943e64bb
Munshi91 <mimunshi91@gmail.com>
2024-05-15 18:45:58 +0600

    Bank Site

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('btn-submit').addEventListener('click', function () {
+  const emailField = document.getElementById('int-email');
+  const email = emailField.value;
+  emailField.value = '';
+  const passwordField = document.getElementById('int-pass');
+  const password = passwordField.value;
+  passwordField.value = '';
+
+  if (email === 'email@gmail.com' && password === '123456') {
+    window.location.href = 'bank.html';
+  } else {
+    alert('Invalid User');
+  }
+});
